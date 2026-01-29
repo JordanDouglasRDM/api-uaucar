5797a9a620407f314fcfc458c097ff23894b50cd
Jordan Douglas <jordandouglas8515@gmail.com>
2026-01-29 12:17:20 -0300

    feat: :boom: adicionar renderização de exceções personalizadas para API
    Incluída a renderização de exceções AuthorizationException, HttpExceptionInterface e UnauthorizedException com formatação padrão para respostas de erro em APIs.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,12 +2,18 @@
 
 declare(strict_types = 1);
 
+use App\Exceptions\UnauthorizedException;
 use App\Http\Middleware\Authenticate;
 use App\Http\Middleware\ResolveTenantMiddleware;
+use App\Http\Utilities\ResponseFormatter;
+use App\Http\Utilities\ServiceResponse;
+use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Routing\Middleware\SubstituteBindings;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -19,14 +25,57 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->group('api', [
             SubstituteBindings::class,
-            'tenant'
+            'tenant',
         ]);
 
         $middleware->alias([
-            'auth' => Authenticate::class,
+            'auth'   => Authenticate::class,
             'tenant' => ResolveTenantMiddleware::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
-    })->create();
+
+        $exceptions->render(function (
+            AuthorizationException $e,
+                                   $request
+        ) {
+            return ResponseFormatter::format(
+                ServiceResponse::error(
+                    throw: $e,
+                    status: Response::HTTP_FORBIDDEN,
+                    message: 'Esta ação não é autorizada.',
+                )
+            );
+        });
+        $exceptions->render(function (
+            HttpExceptionInterface $e,
+                                   $request
+        ) {
+            if ($request->is('api/*') || $request->expectsJson()) {
+                return ResponseFormatter::format(
+                    ServiceResponse::error(
+                        throw: $e,
+                        status: $e->getStatusCode(),
+                        message: __($e->getMessage()) ?: 'Erro na requisição.',
+                    )
+                );
+            }
+
+            return null;
+        });
+
+        $exceptions->render(function (
+            UnauthorizedException $e,
+                                  $request
+        ) {
+            return ResponseFormatter::format(
+                ServiceResponse::error(
+                    throw: $e,
+                    status: Response::HTTP_UNAUTHORIZED,
+                    message: 'Esta ação não é autorizada.',
+                )
+            );
+        });
+
+    })
+    ->create();
